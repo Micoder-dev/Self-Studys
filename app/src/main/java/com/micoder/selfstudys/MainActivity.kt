7e83eac344d33c072659c31904e87639d893b278
Micoder-dev <micoder.com@gmail.com>
2022-09-10 14:31:51 +0530

    smooth bottom nav implemented

--- app/src/main/java/com/micoder/selfstudys/MainActivity.kt
+++ app/src/main/java/com/micoder/selfstudys/MainActivity.kt
@@ -2,10 +2,37 @@ package com.micoder.selfstudys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.micoder.selfstudys.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding // view binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // view binding implementation
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.main_fragment)
+        setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
     }
+
+    // bottom nav bar with smooth nav bar
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav_menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
