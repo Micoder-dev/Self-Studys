db3a5a5e503a00e4054c9adb0babd0e3dde6d0ca
Micoder-dev <micoder.com@gmail.com>
2022-09-10 19:59:51 +0530

    floating navigation view implemented

--- app/src/main/java/com/micoder/selfstudys/MainActivity.kt
+++ app/src/main/java/com/micoder/selfstudys/MainActivity.kt
@@ -15,9 +15,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.andremion.floatingnavigationview.FloatingNavigationView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.snackbar.Snackbar
 import com.micoder.selfstudys.databinding.ActivityMainBinding
 
 
@@ -27,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding // view binding
 
     private var mToolBar: MaterialToolbar? = null
+
+    private lateinit var mFloatingNavigationView: FloatingNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,16 +57,26 @@ class MainActivity : AppCompatActivity() {
         val actionBarView: View = layoutInflater.inflate(R.layout.app_bar_item, null)
         val text = actionBarView.findViewById<TextView>(R.id.actionBarTitle)
 
-        val spannableString = SpannableString("SelfStudy")
-        spannableString.setSpan(ForegroundColorSpan(Color.GREEN), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(ForegroundColorSpan(Color.WHITE), 4, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        val spannableString = SpannableString("SelfStudys")
+        spannableString.setSpan(ForegroundColorSpan(resources.getColor(R.color.liteBlue)), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(resources.getColor(R.color.liteGreen)), 4, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         text.setText(spannableString)
         actionBar?.setCustomView(actionBarView)
 
-
+        // bottom nav
         navController = findNavController(R.id.main_fragment)
         setupActionBarWithNavController(navController)
         setupSmoothBottomMenu()
+
+
+        // Floating Navigation View
+        mFloatingNavigationView = findViewById<View>(R.id.floating_navigation_view) as FloatingNavigationView
+        mFloatingNavigationView.setOnClickListener { mFloatingNavigationView.open() }
+        mFloatingNavigationView.setNavigationItemSelectedListener { item ->
+            Snackbar.make(mFloatingNavigationView.parent as View, item.title.toString() + " Selected!", Snackbar.LENGTH_SHORT).show()
+            mFloatingNavigationView.close()
+            true
+        }
     }
 
     // bottom nav bar with smooth nav bar
@@ -76,5 +90,15 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    // Floating Navigation View
+    override fun onBackPressed() {
+        if (mFloatingNavigationView.isOpened) {
+            mFloatingNavigationView.close()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 
 }
